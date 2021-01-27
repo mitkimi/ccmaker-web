74498e30d19efb22883d20bae28046c74b4e4a4d
tianyuke <mitkimicn@gmail.com>
2021-01-27 16:25:16 +0800

    a start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import styles from './index.less';
+import './index.less';
 
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className="title">Page index</h1>
     </div>
   );
 }
